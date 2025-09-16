5826944ce95cb4d5306144bdd3e9a148c53099ea
AbhirKarande <abhir511@gmail.com>
2025-09-15 21:54:44 -0700

    filename change inside example jobscript

--- octo_drawer_mc10_exp2.sh
+++ octo_drawer_mc10_exp2.sh
@@ -12,5 +12,5 @@
 module purge
 module load apptainer
 
-apptainer exec --bind $PWD,/scratch1/$USER --nv --writable-tmpfs simplerenv.sif /bin/bash -c ". /opt/miniconda/etc/profile.d/conda.sh && conda activate simpler_env && cd /opt/SimplerEnv-Communicative && git fetch && git pull && git checkout drawer_mc10_exp2 &&  cd scripts && chmod +x octo_move_near_visual_matching.sh && cd .. && /opt/SimplerEnv-Communicative/scripts/octo_move_near_visual_matching.sh"
+apptainer exec --bind $PWD,/scratch1/$USER --nv --writable-tmpfs simplerenv.sif /bin/bash -c ". /opt/miniconda/etc/profile.d/conda.sh && conda activate simpler_env && cd /opt/SimplerEnv-Communicative && git fetch && git pull && git checkout drawer_mc10_exp2 &&  cd scripts && chmod +x octo_drawer_visual_matching.sh && cd .. && /opt/SimplerEnv-Communicative/scripts/octo_drawer_visual_matching.sh"
 
